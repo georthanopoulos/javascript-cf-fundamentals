6cd15e2fa30b1927848e93b28fe6a874d42924a6
gthanopoulos <geor.thanopoulos@gmail.com>
2026-07-09 18:24:48 +0300

    "feat: minor comment corrections"

--- ch1-numbers/fahr.js
+++ ch1-numbers/fahr.js
@@ -3,5 +3,5 @@ const celsius = 25;
 const fahrenheit = (celsius * 9 / 5) + 32;
 
 console.log(celsius + "\u00B0C is equal to " + fahrenheit + "\u00B0F");      //B0 einai to kuklaki symbol
-console.log(`${celsius}\u00B0C is equal to ${fahrenheit}\u00B0F`);           //2nd way - Interpolation 
+console.log(`${celsius}\u00B0C is equal to ${fahrenheit}\u00B0F`);           //2nd way - Interpolation - preferred and more readable. use this method!
 
