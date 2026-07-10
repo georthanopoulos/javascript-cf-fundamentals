df0efe4bc640e0473b62f1e392e289978be6aa28
gthanopoulos <geor.thanopoulos@gmail.com>
2026-07-10 22:38:00 +0300

    "feat: comments"

--- ch4-arrays/splice.js
+++ ch4-arrays/splice.js
@@ -2,7 +2,7 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
 //Remove - Delete
 const removedSplice = arr.splice(0, 4);    //results -> [1, 2, 3, 4]  Splice affects the arr (mutable). 
-
+                                           // 1st el. (0) -> startIndex. 2nd el (4) -> numberof el to slice out.
 console.log(arr);                          // because of splice p[erformed above the result is [5, 6, 7, 8]
 console.log(removedSplice);
 
