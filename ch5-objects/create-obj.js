130ca67c6037feac503984ee0c1d325fea1e82ee
gthanopoulos <geor.thanopoulos@gmail.com>
2026-07-17 19:43:09 +0300

    "feat: ch5-objects"

---
+++ ch5-objects/create-obj.js
@@ -0,0 +1,14 @@
+const obj = {}                                    //empty object
+const point = { x: 1, y: 2 }                       //basic way of creating objects
+point.z = 4
+
+const person = {
+    firstname: "Alice",
+    lastneme: "W.",
+    hobbies: [tennis, football, piano],
+    home: { x: 234567.65, y: 334456.99 },
+    geometry: [{ x: 23445, y: 1234.76 }, { x: 123333.55, y: 34567 },
+    { x: 1299333.55, y: 30067 }],
+    active: true,
+    age: 45
+}
